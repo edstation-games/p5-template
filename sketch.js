253769709200a4197e80a72a7f39141086dc1907
naathyk-design <naathyk@gmail.com>
2025-11-19 13:42:24 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+
+  // Example placeholder
+  textSize(20);
+  text("Your game starts here!", 180, 200);
+}
